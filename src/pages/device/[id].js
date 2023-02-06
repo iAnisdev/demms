fcdb54f547c4172b126d57e141b65935d02e43b0
marahmanjs <imarahmandev@gmail.com>
2023-02-07 02:00:19 +0700

    alert added

--- src/pages/device/[id].js
+++ src/pages/device/[id].js
@@ -1,9 +1,8 @@
 import { useRouter } from "next/router";
 import MainLayout from "@/Layout/main";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { db } from "./../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
-
 
 const Device = () => {
   const router = useRouter();
@@ -13,7 +12,10 @@ const Device = () => {
   const devicesCollectionRef = collection(db, "devices", id, "data");
 
   let tableRows = records.map((record, index) => {
-    record.time = new Date(record.timestamp.seconds*1000).toLocaleTimeString("en-US", {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric'})
+    record.time = new Date(record.timestamp.seconds * 1000).toLocaleTimeString(
+      "en-US",
+      { weekday: "short", year: "numeric", month: "short", day: "numeric" }
+    );
 
     return (
       <tr className="table-row" key={index}>
@@ -22,32 +24,38 @@ const Device = () => {
         <td className="table-data">{record.current} amp</td>
         <td className="table-data">{record.voltage} v</td>
       </tr>
-    )
-  })
+    );
+  });
   useEffect(() => {
     (async () => {
       const data = await getDocs(devicesCollectionRef);
       setRecords(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    })()
+    })();
   }, [id]);
-
 
   return (
     <MainLayout>
-        
-      <table className="table">
-        <thead className="table-header">
-          <tr className="table-header-row" >
-          <th className="header__item">#</th>
-          <th className="header__item">Time</th>
-          <th className="header__item">Current</th>
-          <th className="header__item">Voltage</th>
-          </tr>
-        </thead>
-        <tbody className="table-content">
-          {tableRows}
-        </tbody>
-      </table>
+      {records.length > 0 ? (
+        <table className="table">
+          <thead className="table-header">
+            <tr className="table-header-row">
+              <th className="header__item">#</th>
+              <th className="header__item">Time</th>
+              <th className="header__item">Current</th>
+              <th className="header__item">Voltage</th>
+            </tr>
+          </thead>
+          <tbody className="table-content">{tableRows}</tbody>
+        </table>
+      ) : (
+        <>
+          <div className="mt-4 p-4">
+          <div class="alert alert-danger" role="alert">
+            No data available for device <b>{id}</b>
+          </div>
+          </div>
+        </>
+      )}
     </MainLayout>
   );
 };
